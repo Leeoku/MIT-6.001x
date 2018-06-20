d0bc85711d1b0382b6ba1c92eb1e4e1c67adad7b
Leeoku <leeoku@gmail.com>
2018-06-20 01:14:30 -0400

    Add pset 1,2

--- mit week 1 pset3.py
+++ mit week 1 pset3.py
@@ -1,3 +1,6 @@
+#Write a program that prints the longest substring of s in which the letters occur in alphabetical order. For example, if s = 'azcbobobegghakl', then your program should print: Longest substring in alphabetical order is: beggh
+#In the case of ties, print the first substring. For example, if s = 'abcbcd', then your program should print: Longest substring in alphabetical order is: abc
+
 s = 'abcdefghijklmnopqrstuvwxyz'
 #make two strings equal to first char
 
