59c86f77bf7fbc4dd7b659aa66a4630a48805a68
Leeoku <leeoku@gmail.com>
2018-06-29 16:40:27 -0400

    week 3 lesson 6 how many

---
+++ mit week 3 how many.py
@@ -0,0 +1,28 @@
+
+'''First, write a procedure, called how_many, which returns the sum of the number of values associated with a dictionary. For example:
+
+>>> print(how_many(animals))
+6
+'''
+animals = { 'a': ['aardvark'], 'b': ['baboon'], 'c': ['coati']}
+
+animals['d'] = ['donkey']
+animals['d'].append('dog')
+animals['d'].append('dingo')
+
+
+def how_many(aDict):
+    '''
+    aDict: A dictionary, where all the values are lists.
+
+    returns: int, how many values are in the dictionary.
+    '''
+    # Your Code Here
+    values = aDict.values()
+    count = []
+    for num in values:
+        count+=num
+    return count
+
+
+print(how_many(animals))
